77c1ab2b0717f419388db29106ed91e62211a426
Flavio-JS <flaviofilho.js@gmail.com>
2025-04-15 20:46:06 -0300

    created GALLERY page.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,15 @@
+import {
+  faFacebook,
+  faInstagram,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export const Footer = () => {
   return (
     <footer id="footer" className="bg-[#2c0f00] py-12">
       <div className="container mx-auto px-6">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div id="footer-about">
             <h3 className="text-xl font-bold text-[#f37b00] mb-4">
               Projeto Sankofa
@@ -21,30 +28,24 @@ export const Footer = () => {
               Redes Sociais
             </h3>
             <div className="flex space-x-4">
-              <span className="text-[#fefefe] hover:text-[#f37b00] cursor-pointer">
-                <i className="fa-brands fa-instagram text-2xl"></i>
-              </span>
-              <span className="text-[#fefefe] hover:text-[#f37b00] cursor-pointer">
-                <i className="fa-brands fa-facebook text-2xl"></i>
-              </span>
-              <span className="text-[#fefefe] hover:text-[#f37b00] cursor-pointer">
-                <i className="fa-brands fa-youtube text-2xl"></i>
-              </span>
-            </div>
-          </div>
-          <div id="footer-newsletter">
-            <h3 className="text-xl font-bold text-[#f37b00] mb-4">
-              Newsletter
-            </h3>
-            <div className="flex">
-              <input
-                type="email"
-                placeholder="Seu e-mail"
-                className="px-4 py-2 rounded-l-lg w-full text-white placeholder:text-gray-500"
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="text-[#fefefe] hover:text-[#f37b00] cursor-pointer"
+                width={24}
+                height={24}
               />
-              <button className="px-4 py-2 bg-[#ce1d00] text-[#fefefe] rounded-r-lg hover:bg-[#5b1300]">
-                <i className="fa-solid fa-paper-plane"></i>
-              </button>
+              <FontAwesomeIcon
+                icon={faFacebook}
+                className="text-[#fefefe] hover:text-[#f37b00] cursor-pointer"
+                width={24}
+                height={24}
+              />
+              <FontAwesomeIcon
+                icon={faYoutube}
+                className="text-[#fefefe] hover:text-[#f37b00] cursor-pointer"
+                width={24}
+                height={24}
+              />
             </div>
           </div>
         </div>
